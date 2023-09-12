cad409380250ca139bd10608e9b749305a88893e
stheyounger <sj2@penrose.us>
2023-09-12 00:27:23 -0700

    [ri3d] teleop imporvement

--- RoboCode/src/main/java/us/brainstormz/threeDay/ThreeDayTeleOp.kt
+++ RoboCode/src/main/java/us/brainstormz/threeDay/ThreeDayTeleOp.kt
@@ -13,12 +13,23 @@ class ThreeDayTeleOp: OpMode() {
     val hardware = ThreeDayHardware()
     val movement = MecanumDriveTrain(hardware)
 
-    data class DepoState(val liftState: LiftPos, val armState: ArmPos)
-    var depoState = DepoState(LiftPos.Down, ArmPos.In)
+    data class DepoState(val liftState: LiftPos, val armState: ArmPos, val milisSinceStateChange: Long)
+    var previousDepoState = DepoState(LiftPos.Down, ArmPos.In, 0)
+
+    var previousArmPosition = ArmPos.In.position
+
+    var previousLiftPosition = LiftPos.Down.position
+    val liftWaitForArmTimeMilis = 800
+
+    var clawAPosition = false
+    var clawBPosition = false
 
     override fun init() {
         /** INIT PHASE */
         hardware.init(hardwareMap)
+
+        telemetry.addLine("pid: ${hardware.lift.getPIDFCoefficients(DcMotor.RunMode.RUN_TO_POSITION)}")
+        telemetry.addLine("lift tolerance: ${hardware.lift.targetPositionTolerance}")
     }
 
     override fun loop() {
@@ -40,61 +51,92 @@ class ThreeDayTeleOp: OpMode() {
         // Collector
         hardware.collector.power = gamepad1.right_trigger.toDouble() - gamepad1.left_trigger.toDouble()
 
-        //Claw
-        hardware.clawA.position =
-                if (gamepad1.right_bumper) hardware.clawAOpenPos else hardware.clawAClosedPos
-        hardware.clawB.position =
-                if (gamepad1.left_bumper) hardware.clawBOpenPos else hardware.clawBClosedPos
-
         //Depo
-        depoState = when {
+        val depoState = when {
             gamepad1.dpad_down -> {
 //                retracted
-                DepoState(LiftPos.Down, ArmPos.In)
+                DepoState(LiftPos.Down, ArmPos.In, System.currentTimeMillis())
             }
             gamepad1.dpad_left -> {
 //                low
-                DepoState(LiftPos.Low, ArmPos.Out)
+                DepoState(LiftPos.Low, ArmPos.Out, System.currentTimeMillis())
             }
             gamepad1.dpad_up -> {
 //                middle
-                DepoState(LiftPos.Middle, ArmPos.Out)
+                DepoState(LiftPos.Middle, ArmPos.Out, System.currentTimeMillis())
             }
             else -> {
-                depoState
+                previousDepoState
             }
         }
+        previousDepoState = depoState
         telemetry.addLine("Depo State: $depoState")
 
-        //Arm
-        val armPosition = if (gamepad2.left_stick_y != 0.0f) {
-            telemetry.addLine("Manual arm")
-            gamepad2.left_stick_y.toDouble()
-        } else {
-            telemetry.addLine("Arm to depo position")
-            depoState.armState.position
-        }
+        //Claw
+        if (gamepad1.right_bumper && !clawAPosition) clawAPosition = !clawAPosition else clawAPosition
 
-        hardware.leftArm.position = armPosition
-        hardware.rightArm.position = armPosition
+        hardware.clawA.position =
+            if (clawAPosition) {
+                hardware.clawAOpenPos
+            } else {
+                hardware.clawAClosedPos
+            }
+
+        if (gamepad1.left_bumper&& !clawBPosition) clawBPosition = !clawBPosition else clawBPosition
+        hardware.clawB.position =
+            if (clawBPosition) {
+                hardware.clawBOpenPos
+            } else {
+                hardware.clawBClosedPos
+            }
 
         //Lift
         when {
-            gamepad2.right_stick_y != 0.0f -> {
+            gamepad2.right_stick_y !in -0.1..0.1 -> {
                 telemetry.addLine("Manual lift")
                 hardware.lift.mode = DcMotor.RunMode.RUN_WITHOUT_ENCODER
-                hardware.lift.power = gamepad2.right_stick_y.toDouble() * LiftPos.Max.position
+                hardware.lift.power = -gamepad2.right_stick_y.toDouble()
             }
-            gamepad1.b -> {
+            gamepad2.x -> {
 //                reset
                 telemetry.addLine("Zero lift")
                 hardware.lift.mode = DcMotor.RunMode.STOP_AND_RESET_ENCODER
             }
             else -> {
-                telemetry.addLine("Lift to depo position")
                 hardware.lift.mode = DcMotor.RunMode.RUN_TO_POSITION
-                hardware.lift.targetPosition = depoState.liftState.position
+
+                val timeSinceStateStarted = System.currentTimeMillis() - depoState.milisSinceStateChange
+                hardware.lift.targetPosition =
+                    if (depoState.liftState.position <= LiftPos.ArmClearance.position && timeSinceStateStarted < liftWaitForArmTimeMilis) {
+                        telemetry.addLine("Lift waiting for arm. timeSinceStateStarted $timeSinceStateStarted")
+                        previousLiftPosition
+                    } else {
+                        telemetry.addLine("Lift to depo position")
+                        depoState.liftState.position
+                    }
+
+                val liftNeedsToGoDown = depoState.liftState.position < hardware.lift.targetPosition
+                hardware.lift.power = if (liftNeedsToGoDown) 0.2 else 1.0
             }
         }
+        previousLiftPosition = hardware.lift.targetPosition
+        telemetry.addLine("Lift position: ${hardware.lift.currentPosition}")
+
+        //Arm
+        val armPosition = if (gamepad2.left_stick_y != 0.0f) {
+            telemetry.addLine("Manual arm")
+            gamepad2.left_stick_y.toDouble()
+        } else if (hardware.lift.targetPosition < LiftPos.Low.position || hardware.lift.isBusy) {
+            telemetry.addLine("Arm waiting for lift")
+            previousArmPosition
+        } else {
+            telemetry.addLine("Arm to depo position")
+            depoState.armState.position
+        }
+        previousArmPosition = armPosition
+
+        hardware.leftArm.position = armPosition
+        hardware.rightArm.position = armPosition
+
     }
 }
