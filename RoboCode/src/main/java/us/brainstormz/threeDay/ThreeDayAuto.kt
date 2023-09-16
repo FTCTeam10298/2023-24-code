7c387af8130912938fe7df3ea1ffd12aa2ea4784
stheyounger <sj2@penrose.us>
2023-09-16 16:04:53 -0700

    [ri1w] added opencv and all drop options

--- RoboCode/src/main/java/us/brainstormz/threeDay/ThreeDayAuto.kt
+++ RoboCode/src/main/java/us/brainstormz/threeDay/ThreeDayAuto.kt
@@ -3,38 +3,99 @@ package us.brainstormz.threeDay
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode
 import com.qualcomm.robotcore.hardware.DcMotor
+import org.openftc.easyopencv.OpenCvCameraRotation
 import us.brainstormz.hardwareClasses.EncoderDriveMovement
+import us.brainstormz.openCvAbstraction.OpenCvAbstraction
 import us.brainstormz.telemetryWizard.TelemetryConsole
 import us.brainstormz.threeDay.ThreeDayHardware.LiftPos
 import us.brainstormz.threeDay.ThreeDayHardware.ArmPos
+import us.brainstormz.potatoBot.TeamPropDetector.PropPosition
+import us.brainstormz.potatoBot.TeamPropDetector
 
 @Autonomous
 class ThreeDayAuto: LinearOpMode() {
 
     val hardware = ThreeDayHardware()
-    val movement = EncoderDriveMovement(hardware, TelemetryConsole(telemetry))
+    val console = TelemetryConsole(telemetry)
+    val movement = EncoderDriveMovement(hardware, console)
+
+    val opencv = OpenCvAbstraction(this)
+    val teamPropDetector = TeamPropDetector(console)
 
     override fun runOpMode() {
         /** INIT PHASE */
+
         hardware.init(hardwareMap)
+
+        opencv.init(hardwareMap)
+        opencv.internalCamera = false
+        opencv.cameraName = "Webcam 1"
+        opencv.cameraOrientation = OpenCvCameraRotation.SIDEWAYS_LEFT
+        opencv.onNewFrame(teamPropDetector::processFrame)
 
         waitForStart()
         /** AUTONOMOUS  PHASE */
 
-        //move to spike spot
-        movement.driveRobotPosition(power = 1.0, inches = 24.0, smartAccel = true)
-        //drop
-        hardware.autoClaw.position = hardware.autoClawDown
-        sleep(300)
-        hardware.autoClaw.position = hardware.autoClawUp
-        //consistent end position
-        movement.driveRobotStrafe(power = 1.0, inches = 2.0, smartAccel = true)
-        movement.driveRobotTurn(power = 1.0, degree = 90.0, smartAccel = true)
+        val propPosition = teamPropDetector.position
+        opencv.stop()
 
-        //go to backboard
-        movement.driveRobotPosition(power = 1.0, inches = 45.0, smartAccel = true)
-        //linup
-        movement.driveRobotStrafe(power = 1.0, inches = -10.0, smartAccel = true)
+        //Drop on spike
+        when (propPosition) {
+            PropPosition.Left -> {
+                //move to spike spot
+                movement.driveRobotPosition(power = 1.0, inches = 24.0, smartAccel = true)
+                //drop
+                hardware.autoClaw.position = hardware.autoClawDown
+                sleep(300)
+                hardware.autoClaw.position = hardware.autoClawUp
+                //consistent end position
+                movement.driveRobotStrafe(power = 1.0, inches = 2.0, smartAccel = true)
+                movement.driveRobotTurn(power = 1.0, degree = 90.0, smartAccel = true)
+
+                //go to backboard
+                movement.driveRobotPosition(power = 1.0, inches = 45.0, smartAccel = true)
+
+                //linup
+                movement.driveRobotStrafe(power = 1.0, inches = -10.0, smartAccel = true)
+
+            }
+            PropPosition.Center -> {
+                //move to spike spot
+                movement.driveRobotPosition(power = 1.0, inches = 23.0, smartAccel = true)
+                movement.driveRobotTurn(power = 1.0, degree = 90.0, smartAccel = true)
+                //drop
+                hardware.autoClaw.position = hardware.autoClawDown
+                sleep(300)
+                hardware.autoClaw.position = hardware.autoClawUp
+                //consistent end position
+                movement.driveRobotStrafe(power = 1.0, inches = 2.0, smartAccel = true)
+
+                //go to backboard
+                movement.driveRobotPosition(power = 1.0, inches = 45.0, smartAccel = true)
+
+                //linup
+                movement.driveRobotStrafe(power = 1.0, inches = -10.0, smartAccel = true)
+            }
+            PropPosition.Right -> {
+                //move to spike spot
+                movement.driveRobotPosition(power = 1.0, inches = 24.0, smartAccel = true)
+                movement.driveRobotTurn(power = 1.0, degree = 180.0, smartAccel = true)
+                //drop
+                hardware.autoClaw.position = hardware.autoClawDown
+                sleep(300)
+                hardware.autoClaw.position = hardware.autoClawUp
+                //consistent end position
+                movement.driveRobotPosition(power = 1.0, inches = -24.0, smartAccel = true)
+                movement.driveRobotTurn(power = 1.0, degree = -90.0, smartAccel = true)
+
+                //go to backboard
+                movement.driveRobotPosition(power = 1.0, inches = 45.0, smartAccel = true)
+
+                //linup
+                movement.driveRobotStrafe(power = 1.0, inches = 34.0, smartAccel = true)
+            }
+        }
+
         //drop
         moveLiftBlocking(LiftPos.Middle.position)
         hardware.rightArm.position = ArmPos.Out.position
