c5697af679ab65f8a38ce6074193651bb64c5e98
AlephBibleo <gabefergesen@gmail.com>
2023-09-15 22:43:12 -0700

    Gabe: Adds Compilable Vision Pipeline

---
+++ RoboCode/src/main/java/us/brainstormz/potatoBot/potatoBotHardware.kt
@@ -0,0 +1,25 @@
+//package us.brainstormz.potatoBot
+//
+//import com.qualcomm.robotcore.hardware.DcMotor
+//import com.qualcomm.robotcore.hardware.HardwareMap
+//import us.brainstormz.hardwareClasses.MecanumHardware
+//
+//class potatoBotHardware : MecanumHardware {
+//    override lateinit var lFDrive: DcMotor
+//    override lateinit var rFDrive: DcMotor
+//    override lateinit var lBDrive: DcMotor
+//    override lateinit var rBDrive: DcMotor
+//    override lateinit var hwMap: HardwareMap
+//
+//    override fun init(ahwMap: HardwareMap) {
+//
+//        hwMap = ahwMap
+//
+//        // Drivetrain
+//        lFDrive = hwMap["lFDrive"] as DcMotor
+//        rFDrive = hwMap["rFDrive"] as DcMotor
+//        lBDrive = hwMap["lBDrive"] as DcMotor
+//        rBDrive = hwMap["rBDrive"] as DcMotor
+//    }
+//
+//}
