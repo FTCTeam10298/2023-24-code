2140a597523d46ef973e76644a692deed19dd105
AlephBibleo <gabefergesen@gmail.com>
2023-09-29 20:57:31 -0700

    Gabe: Fixes Memory Leak on Prop Detector

--- RoboCode/src/main/java/us/brainstormz/potatoBot/TeamPropDetector.kt
+++ RoboCode/src/main/java/us/brainstormz/potatoBot/TeamPropDetector.kt
@@ -25,9 +25,9 @@ class TeamPropDetector() {
     private val tseThreshold = 135
 
     private val orangePlaces = listOf(
-            Rect(Point(100.0, 240.0), Point(0.0, 100.0)),
-            Rect(Point(210.0, 100.0), Point(110.0, 240.0)),
-            Rect(Point(220.0, 100.0), Point(300.0, 240.0)))
+            Rect(Point(1.0, 2.0), Point(0.0, 1.0)),
+            Rect(Point(2.0, 1.0), Point(1.0, 2.0)),
+            Rect(Point(2.0, 1.0), Point(3.0, 2.0)))
 
     private val regions = listOf(
             PropPosition.Left to orangePlaces[0],
@@ -53,27 +53,27 @@ class TeamPropDetector() {
     private lateinit var submats: List<Pair<PropPosition, Mat>>
     private lateinit var cbFrame: Mat
 
+    private var yCrCb = Mat()
+    private var cb = Mat()
+
     fun processFrame(frame: Mat): Mat {
 
         fun colorInRect(rect: Mat): Int {
             return Core.mean(rect).`val`[0].toInt()
         }
 
-        var yCrCb = Mat()
-        var cb = Mat()
 
         fun inputToCb(input: Mat?): Mat {
             Imgproc.cvtColor(input, yCrCb, Imgproc.COLOR_RGB2YCrCb)
             Core.extractChannel(yCrCb, cb, 1)
             return cb
-            return frame
         }
 
         cbFrame = inputToCb(frame)
 
-//        submats = regions.map {
-//            it.first to cbFrame.submat(it.second)
-//        }
+        submats = regions.map {
+            it.first to cbFrame.submat(it.second)
+        }
 
         var result = PropPosition.Right
         var prevColor = 0
@@ -123,7 +123,7 @@ class ThuUnderstudyTest/** Change Depending on robot */: LinearOpMode() {
         opencv.init(hardwareMap)
         opencv.internalCamera = false
         opencv.cameraName = "Webcam 1" //DEFINE THIS IN HW CONFIG ON HUB!!
-        opencv.cameraOrientation = OpenCvCameraRotation.SIDEWAYS_LEFT
+//        opencv.cameraOrientation = OpenCvCameraRotation.SIDEWAYS_LEFT
         hardwareMap.allDeviceMappings.forEach { m ->
             println("HW: ${m.deviceTypeClass} ${m.entrySet().map{it.key}.joinToString(",")}")
         }
