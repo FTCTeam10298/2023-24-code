e70c7a6b5d86ab51b567174ce4f87a22911a757f
AlephBibleo <gabefergesen@gmail.com>
2023-09-29 20:25:53 -0700

    Gabe: No Crashes in THUnderstudy

--- RoboCode/src/main/java/us/brainstormz/potatoBot/TeamPropDetector.kt
+++ RoboCode/src/main/java/us/brainstormz/potatoBot/TeamPropDetector.kt
@@ -25,20 +25,20 @@ class TeamPropDetector() {
     private val tseThreshold = 135
 
     private val orangePlaces = listOf(
-        Rect(Point(100.0, 240.0), Point(0.0, 100.0)),
-        Rect(Point(210.0, 100.0), Point(110.0, 240.0)),
-        Rect(Point(220.0, 100.0), Point(300.0, 240.0)))
+            Rect(Point(100.0, 240.0), Point(0.0, 100.0)),
+            Rect(Point(210.0, 100.0), Point(110.0, 240.0)),
+            Rect(Point(220.0, 100.0), Point(300.0, 240.0)))
 
     private val regions = listOf(
-        TSEPosition.One to orangePlaces[0],
-        TSEPosition.Two to orangePlaces[1],
-        TSEPosition.Three to orangePlaces[2],
+            PropPosition.Left to orangePlaces[0],
+            PropPosition.Center to orangePlaces[1],
+            PropPosition.Right to orangePlaces[2],
     )
 
     private val colors = listOf(
-        PropPosition.Left to blue,
-        PropPosition.Center to black,
-        PropPosition.Right to red
+            PropPosition.Left to blue,
+            PropPosition.Center to black,
+            PropPosition.Right to red
     )
 
     @Volatile // Volatile since accessed by OpMode thread w/o synchronization
@@ -55,49 +55,57 @@ class TeamPropDetector() {
 
     fun processFrame(frame: Mat): Mat {
 
+        fun colorInRect(rect: Mat): Int {
+            return Core.mean(rect).`val`[0].toInt()
+        }
+
+        var yCrCb = Mat()
+        var cb = Mat()
+
+        fun inputToCb(input: Mat?): Mat {
+            Imgproc.cvtColor(input, yCrCb, Imgproc.COLOR_RGB2YCrCb)
+            Core.extractChannel(yCrCb, cb, 1)
+            return cb
+            return frame
+        }
+
         cbFrame = inputToCb(frame)
 
-        submats = regions.map {
-            it.first to cbFrame.submat(it.second)
-        }
+//        submats = regions.map {
+//            it.first to cbFrame.submat(it.second)
+//        }
 
         var result = PropPosition.Right
         var prevColor = 0
-        submats.forEach {
-            val color = colorInRect(it.second)
-            if (color > prevColor) {
-                prevColor = color
-                result = it.first
-            }
-        }
+//        submats.forEach {
+//            val color = colorInRect(it.second)
+//            if (color > prevColor) {
+//                prevColor = color
+//                result = it.first
+//            }
+//        }
 
         position = result
+//
+//        colors.forEach {
+//            val rect = regions.toMap()[it.first]
+//            Imgproc.rectangle(frame, rect, it.second, 2)
+//        }
+//
+//        console.display(8, "Position: $position")
+//        console.display(9, "Highest Color: $prevColor")
+//
+//        return frame
+//    }
+//
 
-        colors.forEach {
-            val rect = regions.toMap()[it.first]
-            Imgproc.rectangle(frame, rect, it.second, 2)
-        }
-
-        console.display(8, "Position: $position")
-        console.display(9, "Highest Color: $prevColor")
+//
+//    /**
+//     * This function takes the RGB frame, converts to YCrCb,
+//     * and extracts the Cb channel to the cb variable
+//     */
 
         return frame
-    }
-
-    private fun colorInRect(rect: Mat): Int {
-        return Core.mean(rect).`val`[0].toInt()
-    }
-
-    /**
-     * This function takes the RGB frame, converts to YCrCb,
-     * and extracts the Cb channel to the cb variable
-     */
-    private var yCrCb = Mat()
-    private var cb = Mat()
-    private fun inputToCb(input: Mat?): Mat {
-        Imgproc.cvtColor(input, yCrCb, Imgproc.COLOR_RGB2YCrCb)
-        Core.extractChannel(yCrCb, cb, 1)
-        return cb
     }
 }
 
@@ -119,7 +127,7 @@ class ThuUnderstudyTest/** Change Depending on robot */: LinearOpMode() {
         hardwareMap.allDeviceMappings.forEach { m ->
             println("HW: ${m.deviceTypeClass} ${m.entrySet().map{it.key}.joinToString(",")}")
         }
-//        opencv.onNewFrame(tseDetector::processFrame)
+        opencv.onNewFrame(tseDetector::processFrame)
 
         waitForStart()
         /** AUTONOMOUS  PHASE */
