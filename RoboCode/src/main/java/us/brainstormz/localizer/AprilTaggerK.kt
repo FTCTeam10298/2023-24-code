b8648f2546e8cab223431fe798c3d669da01f05c
Gabriel LaFrance Fergesen <gabefergesen@gmail.com>
2023-11-17 22:33:07 -0800

    Gabe: Deleted Java comments in AprilTaggerK

--- RoboCode/src/main/java/us/brainstormz/localizer/AprilTaggerK.kt
+++ RoboCode/src/main/java/us/brainstormz/localizer/AprilTaggerK.kt
@@ -109,28 +109,9 @@ class AprilTagger : LinearOpMode() {
     private fun initAprilTag() {
 
         // Create the AprilTag processor.
+        // There are many options commented out in the Java version, see this for more configurability...
         aprilTag = AprilTagProcessor.Builder() // The following default settings are available to un-comment and edit as needed.
-                //.setDrawAxes(false)
-                //.setDrawCubeProjection(false)
-                //.setDrawTagOutline(true)
-                //.setTagFamily(AprilTagProcessor.TagFamily.TAG_36h11)
-                //.setTagLibrary(AprilTagGameDatabase.getCenterStageTagLibrary())
-                //.setOutputUnits(DistanceUnit.INCH, AngleUnit.DEGREES)
-                // == CAMERA CALIBRATION ==
-                // If you do not manually specify calibration parameters, the SDK will attempt
-                // to load a predefined calibration for your camera.
-                //.setLensIntrinsics(578.272, 578.272, 402.145, 221.506)
-                // ... these parameters are fx, fy, cx, cy.
                 .build()
-
-        // Adjust Image Decimation to trade-off detection-range for detection-rate.
-        // eg: Some typical detection data using a Logitech C920 WebCam
-        // Decimation = 1 ..  Detect 2" Tag from 10 feet away at 10 Frames per second
-        // Decimation = 2 ..  Detect 2" Tag from 6  feet away at 22 Frames per second
-        // Decimation = 3 ..  Detect 2" Tag from 4  feet away at 30 Frames Per Second (default)
-        // Decimation = 3 ..  Detect 5" Tag from 10 feet away at 30 Frames Per Second (default)
-        // Note: Decimation can be changed on-the-fly to adapt during a match.
-        //aprilTag.setDecimation(3);
 
         // Create the vision portal by using a builder.
         val builder = VisionPortal.Builder()
@@ -143,18 +124,18 @@ class AprilTagger : LinearOpMode() {
         }
 
         // Choose a camera resolution. Not all cameras support all resolutions.
-        //builder.setCameraResolution(new Size(640, 480));
+        //...
 
         // Enable the RC preview (LiveView).  Set "false" to omit camera monitoring.
-        //builder.enableLiveView(true);
+        //...
 
         // Set the stream format; MJPEG uses less bandwidth than default YUY2.
-        //builder.setStreamFormat(VisionPortal.StreamFormat.YUY2);
+        //...
 
         // Choose whether or not LiveView stops if no processors are enabled.
         // If set "true", monitor shows solid orange screen if no processors enabled.
         // If set "false", monitor shows camera view without annotations.
-        //builder.setAutoStopLiveView(false);
+        //...
 
         // Set and enable the processor.
         builder.addProcessor(aprilTag)
@@ -163,8 +144,8 @@ class AprilTagger : LinearOpMode() {
         visionPortal = builder.build()
 
         // Disable or re-enable the aprilTag processor at any time.
-        //visionPortal.setProcessorEnabled(aprilTag, true);
-    } // end method initAprilTag()
+        //...
+    } //...
 
     /**
      * Add telemetry about AprilTag detections.
@@ -184,15 +165,15 @@ class AprilTagger : LinearOpMode() {
                 telemetry.addLine(String.format("\n==== (ID %d) Unknown", detection.id))
                 telemetry.addLine(String.format("Center %6.0f %6.0f   (pixels)", detection.center.x, detection.center.y))
             }
-        } // end for() loop
+        } // ...
 
         // Add "key" information to telemetry
         telemetry.addLine("\nkey:\nXYZ = X (Right), Y (Forward), Z (Up) dist.")
         telemetry.addLine("PRY = Pitch, Roll & Yaw (XYZ Rotation)")
         telemetry.addLine("RBE = Range, Bearing & Elevation")
-    } // end method telemetryAprilTag()
+    } //...
 
     companion object {
         private const val USE_WEBCAM = true // true for webcam, false for phone camera
     }
-} // end class
+} // ...
