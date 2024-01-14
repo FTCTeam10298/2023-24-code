f0edc079fd69fa6de5174f7e035c6ad11825272f
stheyounger <qWPw2mr^3sVF%Q>
2024-01-13 21:53:04 -0800

    added encoder position and imu readouts to odom test

--- RoboCode/src/main/java/us/brainstormz/robotTwo/OdometryTest.kt
+++ RoboCode/src/main/java/us/brainstormz/robotTwo/OdometryTest.kt
@@ -2,6 +2,7 @@ package us.brainstormz.robotTwo
 
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous
 import com.qualcomm.robotcore.eventloop.opmode.OpMode
+import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit
 import us.brainstormz.localizer.RRTwoWheelLocalizer
 import us.brainstormz.roadrunner.RoadRunnerTwoDeadWheelLocalizer
 
@@ -14,9 +15,17 @@ class OdometryTest: OpMode() {
         localizer = RRTwoWheelLocalizer(hardware= hardware, inchesPerTick= hardware.inchesPerTick)
     }
     override fun loop() {
+        val perpPos = hardware.perpendicularOdom.getCurrentPosition()
+        val parPos = hardware.parallelOdom.getCurrentPosition()
+        val imuYawDegrees = hardware.imu.robotYawPitchRollAngles.getYaw(AngleUnit.DEGREES)
+        telemetry.addLine("perpPos: $perpPos")
+        telemetry.addLine("parPos: $parPos")
+        telemetry.addLine("imuYawDegrees: $imuYawDegrees")
+
         localizer.recalculatePositionAndRotation()
         val currentPosition = localizer.currentPositionAndRotation()
-        telemetry.addLine("currentPosition: $currentPosition")
+        telemetry.addLine("rr current position: $currentPosition")
+
         telemetry.update()
     }
 }
