24be0b566a378bd966fc2fd6b7ef82ec8b8f00a2
stheyounger <qWPw2mr^3sVF%Q>
2024-01-19 13:26:00 -0800

    added 180 function to axon encoder reader

--- RoboCode/src/main/java/us/brainstormz/robotTwo/AxonEncoderReader.kt
+++ RoboCode/src/main/java/us/brainstormz/robotTwo/AxonEncoderReader.kt
@@ -4,8 +4,13 @@ import com.qualcomm.robotcore.hardware.AnalogInput
 
 
 class AxonEncoderReader(private val axonEncoder: AnalogInput, val angleOffsetDegrees: Double = 0.0) {
+    /** Angle from 0..180 */
+    fun getAngleFrom180Degrees(): Double = getPositionDegrees() % 180
+
+    /** Angle from 0-360 */
     fun getPositionDegrees(): Double = getRawPositionDegrees() + angleOffsetDegrees
 
+    /** Angle from 0-360 */
     fun getRawPositionDegrees(): Double {
         // get the voltage of our analog line
         // divide by 3.3 (the max voltage) to get a value between 0 and 1
