2a038de74e87818a4fb81f396410646057523b6c
stheyounger <qWPw2mr^3sVF%Q>
2024-01-27 16:45:55 -0800

    retract claws when the handoff manager is unsure

--- RoboCode/src/main/java/us/brainstormz/robotTwo/HandoffManager.kt
+++ RoboCode/src/main/java/us/brainstormz/robotTwo/HandoffManager.kt
@@ -92,8 +92,10 @@ class HandoffManager(
                 ClawStateFromHandoff.Retracted
             }
             else -> {
-                telemetry.addLine("Not sure what to do so just doing the same thing as before")
-                previousClawState
+                telemetry.addLine("Not sure what to do, retracting claws")
+                ClawStateFromHandoff.Retracted
+//                telemetry.addLine("Not sure what to do so just doing the same thing as before")
+//                previousClawState
             }
         }
 
@@ -110,67 +112,4 @@ class HandoffManager(
                 clawPosition = clawState
         )
     }
-
-//    var armPosition = Arm.Positions.In
-//    fun transfer() {
-//
-//        val isArmAtAPositionWhichAllowsTheLiftToMoveDown = arm.getArmAngleDegrees() >= Arm.Positions.In.angleDegrees
-//
-//        telemetry.addLine("\n\nMoving collector and claws to the transfer positions")
-//        if (isArmAtAPositionWhichAllowsTheLiftToMoveDown) {
-//            telemetry.addLine("\n\nMoving lift to the transfer position because arm is out of the way")
-//            lift.moveLiftToBottom()
-//        } else {
-//            lift.powerLift(0.0)
-//        }
-//
-//        val liftExtensionIsAllTheWayDown = lift.isLimitSwitchActivated()
-//        if (liftExtensionIsAllTheWayDown) {
-//            collector.moveCollectorAllTheWayIn()
-//        } else {
-//            collector.moveExtendoToPosition(Collector.ExtendoPositions.BeforeTransfer.ticks)
-//        }
-//
-//        val isCollectorAllTheWayIn = collector.isCollectorAllTheWayIn()
-//        val bothExtensionsAreAllTheWayIn = liftExtensionIsAllTheWayDown && isCollectorAllTheWayIn
-//        if (bothExtensionsAreAllTheWayIn) {
-//            telemetry.addLine("\n\nExtensions are ready to transfer")
-//
-//            armPosition = Arm.Positions.In
-//            arm.moveArmTowardPosition(armPosition.angleDegrees)
-//            val isArmReadyToTransfer = arm.getArmAngleDegrees() <= Arm.Positions.ReadyToTransfer.angleDegrees
-//            telemetry.addLine("isArmReadyToTransfer: $isArmReadyToTransfer")
-//            val areRollersReadyToTransfer = true//collector.arePixelsAlignedInTransfer()
-//            telemetry.addLine("areRollersReadyToTransfer: $areRollersReadyToTransfer")
-//
-//            if (isArmReadyToTransfer && areRollersReadyToTransfer) {
-//                telemetry.addLine("\n\nArm and flaps are ready to transfer")
-//
-//                val isPixelInLeftTransfer = true//collector.isPixelIn(leftTransferSensor)
-//                if (isPixelInLeftTransfer) {
-//                    leftClawServo.position = RobotTwoHardware.LeftClawPosition.Gripping.position
-//                }
-//                val isPixelInRightTransfer = true//collector.isPixelIn(rightTransferSensor)
-//                if (isPixelInRightTransfer) {
-//                    rightClawServo.position = RobotTwoHardware.RightClawPosition.Gripping.position
-//                }
-//
-//                lights.setPattern(RevBlinkinLedDriver.BlinkinPattern.CONFETTI)
-//                telemetry.addLine("\n\nTransfer done")
-//            }
-//        } else {
-//            telemetry.addLine("\n\nMoving arm and claws to the transfer position")
-//
-//            armPosition = Arm.Positions.ReadyToTransfer
-//            arm.moveArmTowardPosition(armPosition.angleDegrees)
-//
-//            //|| LiftIsn'tDown
-//            if (collector.isPixelIn(rightTransferSensor) && !isCollectorAllTheWayIn && !liftExtensionIsAllTheWayDown) {
-//                rightClawServo.position = RobotTwoHardware.RightClawPosition.Retracted.position
-//            }
-//            if (collector.isPixelIn(leftTransferSensor) && !isCollectorAllTheWayIn && !liftExtensionIsAllTheWayDown) {
-//                leftClawServo.position = RobotTwoHardware.LeftClawPosition.Retracted.position
-//            }
-//        }
-//    }
 }
