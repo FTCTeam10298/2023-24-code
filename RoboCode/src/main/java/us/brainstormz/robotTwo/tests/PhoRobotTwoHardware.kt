9d1a169f0524ceaa55d735ca1f8598e1b48b8db1
stheyounger <qWPw2mr^3sVF%Q>
2024-02-07 11:48:05 -0800

    added hardware init

--- RoboCode/src/main/java/us/brainstormz/robotTwo/tests/PhoRobotTwoHardware.kt
+++ RoboCode/src/main/java/us/brainstormz/robotTwo/tests/PhoRobotTwoHardware.kt
@@ -1,27 +1,46 @@
 package us.brainstormz.robotTwo.tests
 
-import com.qualcomm.robotcore.hardware.DcMotor
-import com.qualcomm.robotcore.hardware.DcMotorEx
-import com.qualcomm.robotcore.hardware.DcMotorSimple
-import com.qualcomm.robotcore.hardware.HardwareMap
+import com.qualcomm.robotcore.hardware.AnalogInput
 import org.firstinspires.ftc.robotcore.external.Telemetry
+import us.brainstormz.pho.hardware.PhoCRServo
+import us.brainstormz.pho.hardware.PhoMotor
 import us.brainstormz.pho.PhoOpMode
-import us.brainstormz.pho.PrintlnTelemetry
+import us.brainstormz.pho.hardware.PhoAnalogInputController
+import us.brainstormz.pho.hardware.PhoColorSensor
+import us.brainstormz.pho.hardware.PhoServo
 import us.brainstormz.robotTwo.RobotTwoHardware
-import kotlin.reflect.typeOf
 
 class PhoRobotTwoHardware(opmode: PhoOpMode, telemetry:Telemetry): RobotTwoHardware(opmode = opmode, telemetry = telemetry) {
-    /** Don't use ahwMap */
-    override fun init(ahwMap: HardwareMap) {
-        println("Initializing hardware")
+    init {
+        extendoMotorMaster = PhoMotor()
+        extendoMotorSlave = PhoMotor()
+        liftMotorMaster = PhoMotor()
+        liftMotorSlave = PhoMotor()
+        lFDrive = PhoMotor()
+        rFDrive = PhoMotor()
+        lBDrive = PhoMotor()
+        rBDrive = PhoMotor()
 
-        val allFields = this::class.java.fields.toList()
-        val motors = allFields.filter {it ->
-            val type = it.type
-            println("type: $type")
-            it.type is DcMotorSimple
-        }
-        println(motors.size)
+        hangReleaseServo = PhoCRServo()
+        collectorServo1 = PhoCRServo()
+        collectorServo2 = PhoCRServo()
+        collectorServo2 = PhoCRServo()
+        transferDirectorServo = PhoCRServo()
+        leftTransferServo = PhoCRServo()
+        rightTransferServo = PhoCRServo()
+        armServo1 = PhoCRServo()
+        armServo2 = PhoCRServo()
+
+        rightClawServo = PhoServo()
+        leftClawServo = PhoServo()
+
+
+        armEncoder = AnalogInput(PhoAnalogInputController(), 0)
+        leftRollerEncoder = AnalogInput(PhoAnalogInputController(), 0)
+        rightRollerEncoder = AnalogInput(PhoAnalogInputController(), 0)
+
+        leftTransferSensor = PhoColorSensor()
+        rightTransferSensor = PhoColorSensor()
     }
 }
 
--- RoboCode/src/main/java/us/brainstormz/robotTwo/tests/TeleopTests.kt
+++ RoboCode/src/main/java/us/brainstormz/robotTwo/tests/TeleopTests.kt
@@ -3,19 +3,24 @@ package us.brainstormz.robotTwo.tests
 import us.brainstormz.pho.PhoOpMode
 import us.brainstormz.pho.PrintlnTelemetry
 
-class TeleopTests {
-    fun main() {
-        val telemetry = PrintlnTelemetry()
-        telemetry.addLine()
-        telemetry.addLine("Hi there")
-        telemetry.update()
-    }
-}
+//    fun main() {
+//        val telemetry = PrintlnTelemetry()
+//        telemetry.addLine()
+//        telemetry.addLine("Hi there")
+//        telemetry.update()
+//    }
+
+//    fun main() {
+//        val opmode = PhoOpMode(telemetry = PrintlnTelemetry())
+//        opmode.init()
+//        opmode.init_loop()
+//        opmode.start()
+//        opmode.loop()
+//    }
 
 fun main() {
     val opmode = PhoOpMode(telemetry = PrintlnTelemetry())
-    opmode.init()
-    opmode.init_loop()
-    opmode.start()
-    opmode.loop()
+    val hardware = PhoRobotTwoHardware(opmode = opmode, telemetry = opmode.telemetry)
+    println("HI ;lkj")
+    hardware.rightClawServo.position = 0.0
 }
