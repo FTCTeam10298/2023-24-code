19ac0df62a512bfd4614d8a6fd4a7f57bf2e9578
stheyounger <qWPw2mr^3sVF%Q>
2024-02-07 00:20:32 -0800

    add set speed all function to mecanum drive train

--- RoboCode/src/main/java/us/brainstormz/hardwareClasses/MecanumDriveTrain.kt
+++ RoboCode/src/main/java/us/brainstormz/hardwareClasses/MecanumDriveTrain.kt
@@ -1,6 +1,8 @@
 package us.brainstormz.hardwareClasses
 
 import com.qualcomm.robotcore.hardware.DcMotor
+import com.qualcomm.robotcore.util.Range
+import kotlin.math.abs
 import kotlin.math.cos
 import kotlin.math.sin
 
@@ -89,4 +91,43 @@ open class MecanumDriveTrain(private val hardware: MecanumHardware) {
 
     fun driveAllAreBusy(): Boolean = hardware.lFDrive.isBusy && hardware.rFDrive.isBusy && hardware.lBDrive.isBusy && hardware.rBDrive.isBusy
 
+    fun setSpeedAll(vX: Double, vY: Double, vA: Double, minPower: Double, maxPower: Double) {
+
+        // Calculate theoretical values for motor powers using transformation matrix
+        var fl = vY + vX - vA
+        var fr = vY - vX + vA
+        var bl = vY - vX - vA
+        var br = vY + vX + vA
+
+        // Find the largest magnitude of power and the average magnitude of power to scale down to
+        // maxPower and up to minPower
+        var max = abs(fl)
+        max = max.coerceAtLeast(abs(fr))
+        max = max.coerceAtLeast(abs(bl))
+        max = max.coerceAtLeast(abs(br))
+        val ave = (abs(fl) + abs(fr) + abs(bl) + abs(br)) / 4
+        if (max > maxPower) {
+            fl *= maxPower / max
+            bl *= maxPower / max
+            br *= maxPower / max
+            fr *= maxPower / max
+        } else if (ave < minPower) {
+            fl *= minPower / ave
+            bl *= minPower / ave
+            br *= minPower / ave
+            fr *= minPower / ave
+        }
+
+        // Range clip just to be safe
+        fl = Range.clip(fl, -1.0, 1.0)
+        fr = Range.clip(fr, -1.0, 1.0)
+        bl = Range.clip(bl, -1.0, 1.0)
+        br = Range.clip(br, -1.0, 1.0)
+
+//        telemetry.addLine("Powers: $fl, $bl, $fr, $br" )
+        println("Powers: $fl, $bl, $fr, $br")
+
+        // Set powers
+        driveSetPower(fl, fr, bl, br)
+    }
 }
