933ccd50090023ba303a80599142c3db88568f38
stheyounger <qWPw2mr^3sVF%Q>
2024-02-07 12:31:23 -0800

    ran a test to check whether launcher control worked
    launcher control works

--- RoboCode/src/main/java/us/brainstormz/robotTwo/tests/TeleopTests.kt
+++ RoboCode/src/main/java/us/brainstormz/robotTwo/tests/TeleopTests.kt
@@ -5,25 +5,35 @@ import us.brainstormz.faux.FauxOpMode
 import us.brainstormz.faux.PrintlnTelemetry
 import us.brainstormz.robotTwo.RobotTwoTeleOp
 
-//    fun main() {
-//        val telemetry = PrintlnTelemetry()
-//        telemetry.addLine()
-//        telemetry.addLine("Hi there")
-//        telemetry.update()
-//    }
-
-//    fun main() {
-//        val opmode = PhoOpMode(telemetry = PrintlnTelemetry())
-//        opmode.init()
-//        opmode.init_loop()
-//        opmode.start()
-//        opmode.loop()
-//    }
-
 fun main() {
     val opmode = FauxOpMode(telemetry = PrintlnTelemetry())
     val hardware = FauxRobotTwoHardware(opmode = opmode, telemetry = opmode.telemetry)
     val teleop = RobotTwoTeleOp(hardware, opmode.telemetry)
+
+
+    //Init
+    val gamepad1 = Gamepad()
+
+    val gamepad2 = Gamepad()
+
+    //Action 1
     teleop.init()
-    teleop.loop(Gamepad(), Gamepad())
+    teleop.loop(gamepad1, gamepad2)
+
+    val initState = teleop.functionalReactiveAutoRunner.previousTargetState
+
+    //Change
+    gamepad1.y = true
+
+    //Action 2
+    teleop.loop(gamepad1, gamepad2)
+
+    //Result
+    val finalState = teleop.functionalReactiveAutoRunner.previousTargetState
+
+    println("initState: $initState")
+    println("finalState: $finalState")
+
+    val areTheyTheSame = initState==finalState
+    println("areTheyTheSame: $areTheyTheSame")
 }
