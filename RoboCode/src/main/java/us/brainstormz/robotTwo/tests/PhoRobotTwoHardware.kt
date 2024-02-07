57b7390e626d1bc1a86d1c28c604e8bdcab51add
stheyounger <qWPw2mr^3sVF%Q>
2024-02-07 10:21:37 -0800

    started working on field initialization in robot two hardware

--- RoboCode/src/main/java/us/brainstormz/robotTwo/tests/PhoRobotTwoHardware.kt
+++ RoboCode/src/main/java/us/brainstormz/robotTwo/tests/PhoRobotTwoHardware.kt
@@ -1,10 +1,27 @@
 package us.brainstormz.robotTwo.tests
 
+import com.qualcomm.robotcore.hardware.DcMotor
+import com.qualcomm.robotcore.hardware.DcMotorEx
+import com.qualcomm.robotcore.hardware.DcMotorSimple
+import com.qualcomm.robotcore.hardware.HardwareMap
+import org.firstinspires.ftc.robotcore.external.Telemetry
 import us.brainstormz.pho.PhoOpMode
 import us.brainstormz.pho.PrintlnTelemetry
 import us.brainstormz.robotTwo.RobotTwoHardware
+import kotlin.reflect.typeOf
 
-//class PhoRobotTwoHardware: RobotTwoHardware(opmode = PhoOpMode(), telemetry = PrintlnTelemetry() ) {
-//
-//}
+class PhoRobotTwoHardware(opmode: PhoOpMode, telemetry:Telemetry): RobotTwoHardware(opmode = opmode, telemetry = telemetry) {
+    /** Don't use ahwMap */
+    override fun init(ahwMap: HardwareMap) {
+        println("Initializing hardware")
+
+        val allFields = this::class.java.fields.toList()
+        val motors = allFields.filter {it ->
+            val type = it.type
+            println("type: $type")
+            it.type is DcMotorSimple
+        }
+        println(motors.size)
+    }
+}
 
