6b55021f3577c722e2f3020917926544f1f84e53
stheyounger <qWPw2mr^3sVF%Q>
2024-02-07 13:46:50 -0800

    launcher test

--- RoboCode/src/main/java/us/brainstormz/robotTwo/tests/TeleopTests.kt
+++ RoboCode/src/main/java/us/brainstormz/robotTwo/tests/TeleopTests.kt
@@ -1,39 +1,136 @@
 package us.brainstormz.robotTwo.tests
 
+import android.text.BoringLayout
 import com.qualcomm.robotcore.hardware.Gamepad
 import us.brainstormz.faux.FauxOpMode
 import us.brainstormz.faux.PrintlnTelemetry
+import us.brainstormz.localizer.PositionAndRotation
+import us.brainstormz.robotTwo.ActualRobot
+import us.brainstormz.robotTwo.ActualWorld
+import us.brainstormz.robotTwo.Arm
+import us.brainstormz.robotTwo.Claw
+import us.brainstormz.robotTwo.CollectorSystem
+import us.brainstormz.robotTwo.CollectorTarget
+import us.brainstormz.robotTwo.DepoManager
+import us.brainstormz.robotTwo.DepoTarget
+import us.brainstormz.robotTwo.Lift
+import us.brainstormz.robotTwo.RobotTwoHardware
 import us.brainstormz.robotTwo.RobotTwoTeleOp
+import us.brainstormz.robotTwo.TargetRobot
+import us.brainstormz.robotTwo.TargetWorld
+import us.brainstormz.robotTwo.tests.TeleopTest.getChangedGamepad
 
 fun main() {
-    val opmode = FauxOpMode(telemetry = PrintlnTelemetry())
-    val hardware = FauxRobotTwoHardware(opmode = opmode, telemetry = opmode.telemetry)
-    val teleop = RobotTwoTeleOp(hardware, opmode.telemetry)
+    val steps = listOf(
+            TeleopTest.emptyWorld,
+            TeleopTest.emptyWorld.copy(
+                    actualGamepad1= getChangedGamepad { it.y = true },
+            )
+    )
+    val expectedOutput = TeleopTest.emptyTarget.copy(
+            targetRobot = TeleopTest.emptyTarget.targetRobot.copy(
+                    launcherPosition = RobotTwoHardware.LauncherPosition.Released
+            )
+    )
+
+    val result = TeleopTest.runTest(steps)
+    val testPass = TeleopTest.didTestPass(result = result) { output ->
+        output.targetRobot.launcherPosition == expectedOutput.targetRobot.launcherPosition
+    }
+
+    println(result)
+    println("Test passed? $testPass")
+}
 
 
-    //Init
-    val gamepad1 = Gamepad()
+object TeleopTest {
+    fun runTest(testSteps: List<ActualWorld>): List<Pair<ActualWorld, TargetWorld>> {
+        val opmode = FauxOpMode(telemetry = PrintlnTelemetry())
+        val hardware = FauxRobotTwoHardware(opmode = opmode, telemetry = opmode.telemetry)
+        val teleop = RobotTwoTeleOp(hardware, opmode.telemetry)
 
-    val gamepad2 = Gamepad()
+        val results:List<Pair<ActualWorld, TargetWorld>> = testSteps.mapIndexed() {index, actualWorld ->
+            //Set Init Inputs
+            hardware.actualRobot = actualWorld.actualRobot
 
-    //Action 1
-    teleop.init()
-    teleop.loop(gamepad1, gamepad2)
+            //Run Once
+            teleop.loop(gamepad1 = actualWorld.actualGamepad1, gamepad2 = actualWorld.actualGamepad2)
 
-    val initState = teleop.functionalReactiveAutoRunner.previousTargetState
+            //Get result
+            val result = teleop.functionalReactiveAutoRunner.previousTargetState!!
+            actualWorld to result
+        }
 
-    //Change
-    gamepad1.y = true
+        return results
+    }
+    fun didTestPass(result: List<Pair<ActualWorld, TargetWorld>>, passCondition: (TargetWorld)->Boolean): Boolean {// List<Pair<Boolean, Pair<ActualWorld, TargetWorld>>> {
+        return passCondition(result.last().second)
+    }
 
-    //Action 2
-    teleop.loop(gamepad1, gamepad2)
+    val emptySensorReading = CollectorSystem.SensorReading(0, 0, 0 ,0)
+    val emptyWorld = ActualWorld(
+            actualGamepad1= Gamepad(),
+            actualGamepad2 = Gamepad(),
+            actualRobot = ActualRobot(
+                    positionAndRotation = PositionAndRotation(),
+                    depoState = DepoManager.ActualDepo(
+                            armAngleDegrees = 0.0,
+                            liftPositionTicks = 0
+                    ),
+                    collectorSystemState = CollectorSystem.ActualCollector(
+                            extendoPositionTicks = 0,
+                            leftRollerAngleDegrees = 0.0,
+                            rightRollerAngleDegrees = 0.0,
+                            leftTransferState = emptySensorReading,
+                            rightTransferState = emptySensorReading,
+                    ),
+            ),
+            timestampMilis = 0,
+    )
+    val noInput = RobotTwoTeleOp.DriverInput(
+            driveVelocity = PositionAndRotation(),
+            depo = RobotTwoTeleOp.DepoInput.NoInput,
+            leftClaw = RobotTwoTeleOp.ClawInput.NoInput,
+            rightClaw = RobotTwoTeleOp.ClawInput.NoInput,
+            collector = RobotTwoTeleOp.CollectorInput.NoInput,
+            rollers = RobotTwoTeleOp.RollerInput.NoInput,
+            extendo = RobotTwoTeleOp.ExtendoInput.NoInput,
+            handoff = RobotTwoTeleOp.HandoffInput.NoInput,
+            hang = RobotTwoTeleOp.HangInput.NoInput,
+            launcher = RobotTwoTeleOp.LauncherInput.NoInput,
+            bumperMode = RobotTwoTeleOp.Gamepad1BumperMode.Collector,
+            lightInput = RobotTwoTeleOp.LightInput.NoInput
+    )
+    val emptyTarget = TargetWorld(
+            targetRobot = TargetRobot(
+                    positionAndRotation = PositionAndRotation(),
+                    depoTarget = DepoTarget(
+                            liftPosition = Lift.LiftPositions.Manual,
+                            armPosition = Arm.Positions.Manual,
+                            leftClawPosition = Claw.ClawTarget.Gripping,
+                            rightClawPosition = Claw.ClawTarget.Gripping,
+                    ),
+                    collectorTarget = CollectorTarget(
+                            intakeNoodles = CollectorSystem.CollectorPowers.Off,
+                            rollers = CollectorSystem.RollerState(CollectorSystem.RollerPowers.Off, CollectorSystem.RollerPowers.Off, CollectorSystem.DirectorState.Off),
+                            extendoPositions = CollectorSystem.ExtendoPositions.Manual,
+                    ),
+                    hangPowers = RobotTwoHardware.HangPowers.Holding,
+                    launcherPosition = RobotTwoHardware.LauncherPosition.Holding,
+                    lights = RobotTwoTeleOp.LightTarget(
+                            targetColor = RobotTwoTeleOp.PixelColor.Unknown,
+                            pattern = RobotTwoTeleOp.BothPixelsWeWant(RobotTwoTeleOp.PixelColor.Unknown, RobotTwoTeleOp.PixelColor.Unknown),
+                            timeOfColorChangeMilis = 0L
+                    ),
+            ),
+            isLiftEligableForReset = false,
+            doingHandoff = false,
+            driverInput = noInput,
+            isTargetReached = {_, _ -> false}
+    )
 
-    //Result
-    val finalState = teleop.functionalReactiveAutoRunner.previousTargetState
-
-    println("initState: $initState")
-    println("finalState: $finalState")
-
-    val areTheyTheSame = initState==finalState
-    println("areTheyTheSame: $areTheyTheSame")
+    fun getChangedGamepad(originalGamepad: Gamepad = Gamepad(), gamepadChange: (gamepad: Gamepad)->Unit): Gamepad {
+        gamepadChange(originalGamepad)
+        return originalGamepad
+    }
 }
