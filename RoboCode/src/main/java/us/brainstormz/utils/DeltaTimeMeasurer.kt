b41df9bc50a50d29983b522fc281d1b4854b5dbb
stheyounger <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>
2024-02-19 18:51:02 -0800

    Fixed a memory leak in DeltaTimeMeasurer

--- RoboCode/src/main/java/us/brainstormz/utils/DeltaTimeMeasurer.kt
+++ RoboCode/src/main/java/us/brainstormz/utils/DeltaTimeMeasurer.kt
@@ -1,21 +1,32 @@
 package us.brainstormz.utils
 
+class AverageAccumulator(
+        private var numMeasurements:Long = 0,
+        private var total:Long = 0,
+){
+    fun addMeasurment(quantity:Long){
+        this.numMeasurements += 1
+        this.total += quantity
+    }
+
+    fun getAverage(): Long {
+        return total/numMeasurements
+    }
+}
+
 class DeltaTimeMeasurer {
     private var timeBegin: Long? = null
     private var peakDeltaTime = 0L
     private var deltaTime = 0L
-
-    private val loopTimeAverageStorage = mutableListOf<Long>()
+    private val avg = AverageAccumulator()
 
     fun peakDeltaTime() = this.peakDeltaTime
 
-    fun getAverageLoopTimeMillis(): Long {
-        return loopTimeAverageStorage.sum()/loopTimeAverageStorage.size
-    }
+    fun getAverageLoopTimeMillis() = avg.getAverage()
 
     fun measureTimeSinceLastCallMillis(): Long {
         val deltaTime = endMeasureDT()
-        loopTimeAverageStorage.add(deltaTime)
+        avg.addMeasurment(deltaTime)
         beginMeasureDT()
         return deltaTime
     }
