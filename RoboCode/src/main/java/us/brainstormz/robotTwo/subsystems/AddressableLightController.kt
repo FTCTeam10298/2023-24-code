4d9aa5df8a78fc38b3d7b23943567cbd6f922520
stheyounger <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>
2024-02-29 23:06:38 -0800

    added namedd colors example

--- RoboCode/src/main/java/us/brainstormz/robotTwo/subsystems/AddressableLightController.kt
+++ RoboCode/src/main/java/us/brainstormz/robotTwo/subsystems/AddressableLightController.kt
@@ -1,7 +1,12 @@
 package us.brainstormz.robotTwo.subsystems
 
 interface AddressableLightController {
-    interface RGB
+    data class RGB(val r: Int,val g: Int,val b: Int)
+
+    enum class NamedColors(val rgb: RGB) {
+        Red(RGB(r=255, 0, 0)),
+        Green(RGB(0, g=255, 0))
+    }
 
     /** Function takes no longer than 5ms */
     fun updateLightHalves(rightHalfColor: RGB, leftHalfColor: RGB)
