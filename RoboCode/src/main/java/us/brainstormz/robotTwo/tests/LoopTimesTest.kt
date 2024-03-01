b10097ae9d94b2ca639fa7ef1be352d5e3d78184
stheyounger <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>
2024-02-29 22:34:28 -0800

    more work on loop times test

--- RoboCode/src/main/java/us/brainstormz/robotTwo/tests/LoopTimesTest.kt
+++ RoboCode/src/main/java/us/brainstormz/robotTwo/tests/LoopTimesTest.kt
@@ -3,9 +3,34 @@ package us.brainstormz.robotTwo.tests
 import us.brainstormz.faux.FauxOpMode
 import us.brainstormz.faux.PrintlnTelemetry
 import us.brainstormz.robotTwo.RobotTwoTeleOp
+import us.brainstormz.utils.DataClassHelper
 import us.brainstormz.utils.DeltaTimeMeasurer
 
+fun getHeapInfoAsString(env: Runtime): String {
+    return """
+    |   Max Heap Size = maxMemory() = ${env.maxMemory()}
+    |   Current Heap Size = totalMemory() = ${env.totalMemory()}
+    |   Available in Current Heap = freeMemory() = ${env.freeMemory()} //current heap will extend if no more freeMemory to a maximum of maxMemory
+    |   Currently Used Heap = ${env.totalMemory() - env.freeMemory()}
+    |   Unassigned Heap = ${env.maxMemory() - env.totalMemory()}
+    |   Currently Totally Available Heap Space = ${env.maxMemory() - env.totalMemory() + env.freeMemory()} //available=unassigned + free
+    """.trimMargin()
+}
+
+data class PeakData(
+        val loopTimeMillis: Long,
+        val timeSinceStartMillis: Long,
+        val memoryData: String,
+        val previousLoopMemoryData: String,
+) {
+    override fun toString(): String = DataClassHelper.dataClassToString(this)
+}
+
+var previousLoopMemoryData = ""
 fun main() {
+    val env = Runtime.getRuntime()
+
+
     val opmode = FauxOpMode(telemetry = PrintlnTelemetry())
     val hardware = FauxRobotTwoHardware(opmode = opmode, telemetry = opmode.telemetry)
     val teleop = RobotTwoTeleOp(opmode.telemetry)
@@ -13,7 +38,7 @@ fun main() {
     teleop.init(hardware)
 
     val loopTimeMeasurer = DeltaTimeMeasurer()
-    val peaks = mutableListOf<Pair<Long, Long>>()
+    val peaks = mutableListOf<PeakData>()
 
     val timeToRunLoopMillis = 150000
 //    val timeToRunLoopMillis = 10000
@@ -27,21 +52,28 @@ fun main() {
         //Run
         teleop.loop(gamepad1 = actualWorld.actualGamepad1, gamepad2 = actualWorld.actualGamepad2, hardware)
 
-
         val timeSinceStartMillis = System.currentTimeMillis() - timeStarted
 
         val loopTime = loopTimeMeasurer.measureTimeSinceLastCallMillis()
         val peakTime = loopTimeMeasurer.peakDeltaTime()
 
+        val memoryData = getHeapInfoAsString(env)
         if (loopTime == peakTime) {
-            peaks.add(peakTime to timeSinceStartMillis)
+            peaks.add(PeakData(
+                    loopTimeMillis = loopTime,
+                    timeSinceStartMillis = timeSinceStartMillis,
+                    memoryData = memoryData,
+                    previousLoopMemoryData= previousLoopMemoryData))
         }
 
         println("peak time: ${peakTime}")
 
+        previousLoopMemoryData = memoryData
     } while (timeSinceStartMillis < timeToRunLoopMillis)
 
     println("\n\nnumber of peaks: ${peaks.size}")
-    println("peaks: ${peaks}")
+    println("peaks: ${peaks.fold("") {acc, it ->
+        acc + "\n\n" + it.toString()
+    }}")
     println("highest peak time: ${loopTimeMeasurer.peakDeltaTime()}")
 }
