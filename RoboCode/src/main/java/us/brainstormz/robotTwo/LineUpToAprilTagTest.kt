05f1610e68ef36221c16e249e5fdccf8fbc157a2
stheyounger <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>
2024-03-01 23:07:58 -0800

    added AprilTagLineup class

--- RoboCode/src/main/java/us/brainstormz/robotTwo/LineUpToAprilTagTest.kt
+++ RoboCode/src/main/java/us/brainstormz/robotTwo/LineUpToAprilTagTest.kt
@@ -1,8 +1,11 @@
 package us.brainstormz.robotTwo
 
 import android.util.Size
+import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
+import com.fasterxml.jackson.module.kotlin.readValue
 import com.qualcomm.robotcore.eventloop.opmode.OpMode
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp
+import com.qualcomm.robotcore.hardware.Gamepad
 import com.qualcomm.robotcore.hardware.HardwareMap
 import org.firstinspires.ftc.robotcore.external.Telemetry
 import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName
@@ -10,6 +13,14 @@ import org.firstinspires.ftc.vision.VisionPortal
 import org.firstinspires.ftc.vision.apriltag.AprilTagDetection
 import org.firstinspires.ftc.vision.apriltag.AprilTagPoseFtc
 import org.firstinspires.ftc.vision.apriltag.AprilTagProcessor
+import us.brainstormz.faux.FauxLocalizer
+import us.brainstormz.localizer.PositionAndRotation
+import us.brainstormz.localizer.RRLocalizer
+import us.brainstormz.localizer.RRTwoWheelLocalizer
+import us.brainstormz.pid.PID
+import us.brainstormz.robotTwo.subsystems.Drivetrain
+import us.brainstormz.robotTwo.subsystems.DualMovementModeSubsystem
+import us.brainstormz.utils.ConfigServer
 
 fun formatFtcPose(ftcPose: AprilTagPoseFtc): String = """
    |AprilTagPoseFtc(
@@ -22,14 +33,14 @@ fun formatFtcPose(ftcPose: AprilTagPoseFtc): String = """
    |    yaw= ${ftcPose.yaw},
    |    pitch= ${ftcPose.pitch},
    |    roll= ${ftcPose.roll},)
-""".trimIndent()
+""".trimMargin()
 
 fun aprilTagDetectionToString(tag: AprilTagDetection): String = """
         AprilTagDetection(
         id= ${tag.id},
         ftcPose= ${formatFtcPose(tag.ftcPose)},
         frameAcquisitionNanoTime= ${tag.frameAcquisitionNanoTime})
-    """.trimIndent()
+    """.trimMargin()
 
 
 class AprilTagPipeline(val cameraName: String, val resolution: Size) {
@@ -59,27 +70,109 @@ class AprilTagPipeline(val cameraName: String, val resolution: Size) {
     }
 
     fun detections() = aprilTag?.detections ?: emptyList()
-//    fun resumeStreaming() {
-//        visionPortal!!.resumeStreaming()
-//    }
 
 }
 
+class AprilTagLineup {
+    fun lineUpToTag(currentPosition: PositionAndRotation, previousPosition: PositionAndRotation, aprilTagReading: AprilTagDetection?, previousAprilTagReading: AprilTagDetection?): PositionAndRotation {
+        return if (aprilTagReading != previousAprilTagReading && aprilTagReading != null) {
+            previousPosition.copy(x = currentPosition.x - aprilTagReading.ftcPose.x)
+        } else {
+            previousPosition
+        }
+    }
+}
+
+data class LineUpToAprilTagConfig(
+        val sidewaysPID: PidConfig,
+)
 
 class LineUpToAprilTagTest(private val hardware: RobotTwoHardware, private val telemetry: Telemetry, private val aprilTagPipeline: AprilTagPipeline) {
 
-    private val detectionWeWantId = 6
-    fun loop() {
+    private val aprilTagLineup: AprilTagLineup = AprilTagLineup()
+
+    private val drivetrain = Drivetrain(hardware, RRTwoWheelLocalizer(hardware, hardware.inchesPerTick), telemetry)
+//
+//    private lateinit var config: LineUpToAprilTagConfig
+//    private lateinit var configServer: ConfigServer
+//    fun init() {
+//        config = LineUpToAprilTagConfig(
+//                PidConfig(sidewaysPID)
+//        )
+//        configServer = ConfigServer(
+//                port = 8083,
+//                get = { jacksonObjectMapper().writerWithDefaultPrettyPrinter().writeValueAsString(config) },
+//                update = {
+//                    config = jacksonObjectMapper().readValue(it)
+//                    sidewaysPID = config.sidewaysPID.toPID()
+//                },
+//                getInfoToPrint = {"No Telemetry Passed"})
+//    }
+
+    var sidewaysPID = PID("sidewaysPID",
+                            kp = 0.12,
+                            ki = 1.0E-6,
+                            kd = 1.0
+    )
+
+    var previousDetectionWeWant: AprilTagDetection? = null
+    var previousTarget = Drivetrain.DrivetrainTarget(PositionAndRotation())
+    private val detectionWeWantId = 5
+    fun loop(gamepad1: Gamepad) {
+        drivetrain.xTranslationPID = sidewaysPID
         val currentDetections: List<AprilTagDetection> = aprilTagPipeline.detections()
 
         val detectionWeWant: AprilTagDetection? = currentDetections.firstOrNull {
             it.id == detectionWeWantId
         }
+        
+        val currentPosition = drivetrain.getPosition()
+
+        val targetPosition = aprilTagLineup.lineUpToTag(
+                previousPosition = previousTarget.targetPosition,
+                currentPosition = currentPosition,
+                aprilTagReading = detectionWeWant,
+                previousAprilTagReading = previousDetectionWeWant,
+        )
+
+        telemetry.addLine("\n\ntargetPosition: ${targetPosition}")
+        telemetry.addLine("currentPosition: ${currentPosition}")
+        val positionError = currentPosition - targetPosition
+        telemetry.addLine("positionError: ${positionError}\n\n")
+
+
+        val driverControlPower = Drivetrain.DrivetrainPower(
+                x= gamepad1.left_stick_x.toDouble(),
+                y= -gamepad1.left_stick_y.toDouble(),
+                r= gamepad1.right_stick_x.toDouble())
+
+        val drivetrainTarget = Drivetrain.DrivetrainTarget(
+                targetPosition = targetPosition,
+//                power = driverControlPower,
+//                movementMode =
+//                if ((driverControlPower.x+driverControlPower.y+driverControlPower.y) > 0.1) {
+//                    DualMovementModeSubsystem.MovementMode.Power
+//                } else {
+//                    DualMovementModeSubsystem.MovementMode.Position
+//                }
+        )
+
+        drivetrain.actuateDrivetrain(
+                target = drivetrainTarget,
+                actualPosition = currentPosition,
+                previousTarget = previousTarget)
 
         telemetry.addLine("All detections: ${currentDetections.fold("") {acc, it -> "$acc \n${aprilTagDetectionToString(it)}"}}")
         telemetry.addLine("\n\nOur detection: ${detectionWeWant?.let { aprilTagDetectionToString(it) }}")
         telemetry.update()
+        previousTarget = drivetrainTarget
+        previousDetectionWeWant = detectionWeWant
     }
+
+
+//    fun stop() {
+//        configServer.stop()
+//    }
 }
 
 @TeleOp
@@ -94,10 +187,14 @@ class LineUpToAprilTagTestOpMode: OpMode() {
         aprilTagPipeline.init(null, hardwareMap)
 
         program = LineUpToAprilTagTest(hardware, telemetry, aprilTagPipeline)
+//        program.init()
     }
 
     override fun loop() {
-        program.loop()
+        program.loop(gamepad1)
     }
 
+    override fun stop() {
+//        program.stop()
+    }
 }
