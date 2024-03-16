e35f296504629662642b32309e29822cc57fd2c3
Gabriel LaFrance Fergesen <gabefergesen@gmail.com>
2024-03-15 23:14:30 -0700

    Gabe: offers a bit of documentation for two classes

--- RoboCode/src/main/java/us/brainstormz/localizer/aprilTagLocalization/Foo.kt
+++ RoboCode/src/main/java/us/brainstormz/localizer/aprilTagLocalization/Foo.kt
@@ -9,7 +9,14 @@ import org.firstinspires.ftc.vision.apriltag.AprilTagProcessor
 
 class Foo(private val name:String, val size:Size?) {
     private var aprilTag: AprilTagProcessor? = null
+    //aprilTagProcessor- builder for an AprilTag Processor
+    //A processor is basically a pipeline—it implements an init, processFrame, and OnDrawFrame.
+    //Fun fact: FTC wrote a new vision library! Yeah, no. Nobody cares, just use EasyOpenCV because it's better documented.
+
     private var visionPortal: VisionPortal? = null
+    //Seems to control the camera preview on bot, giving processors a window for rendering.
+    //Does not offer camera rotation options
+    //Seems to correlate processors with preview windows... init frame will show all running processors??
 
     fun init(viewContainerId:Int?, hardwareMap:HardwareMap) {
 
