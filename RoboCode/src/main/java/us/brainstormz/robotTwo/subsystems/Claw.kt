8edc3f4fc162957de7b0febd6606696ed738d617
stheyounger <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>
2024-04-08 19:17:44 -0700

    increased claw open angle

--- RoboCode/src/main/java/us/brainstormz/robotTwo/subsystems/Claw.kt
+++ RoboCode/src/main/java/us/brainstormz/robotTwo/subsystems/Claw.kt
@@ -23,7 +23,7 @@ import kotlin.math.sign
 
 class Claw(private val telemetry: Telemetry) {
     enum class ClawTarget(val angleDegrees: Double) {
-        Gripping(119.2),
+        Gripping(122.0),
         Retracted(0.0)
     }
 
